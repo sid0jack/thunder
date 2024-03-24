df6fcda8ebb2bd852a521bb9776aa486672a423b
Siddhant Nayak <username@users.noreply.github.com>
2024-03-24 14:36:06 +0530

    init repo and some code

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::io;
+fn main() -> io::Result<()>{
+    let nic = tun_tap::Iface::new("thunder0", tun_tap::Mode::Tun).expect("Failed to create TUN device");
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf).expect("Failed to read from TUN device");
+    eprint!("Read {} bytes from TUN device\n", nbytes);        
+    Ok(())
+}
